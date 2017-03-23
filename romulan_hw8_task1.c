b06c96144abb2f9ea19537f3c2e6a1ced8e5ecf5
Chris Werner <chris.lex.werner@gmail.com>
2017-03-23 16:16:16 +0000

    first commit of task1

---
+++ romulan_hw8_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  romulan_hw8_task1.c
+ *       	Usage:  ./romulan_hw8_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:15:25 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chris Werner (), christopherwerner@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>		/* For Standard Library*/
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
