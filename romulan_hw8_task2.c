26fa280dba1012223c3837817a443f65330c272a
Sterling Woodruff <sterlingwoodruff@mail.weber.edu>
2017-03-23 16:16:13 +0000

    Initial commit task2

---
+++ romulan_hw8_task2.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  romulan_hw8_task2.c
+ *       	Usage:  ./romulan_hw8_task2.c
+ *    Description:  Task2
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:15:31 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sterling Woodruff, Chris Werner
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main (int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
