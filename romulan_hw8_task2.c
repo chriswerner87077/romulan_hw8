ba5d48a2ca89fa898cb19e7d89738e4e1261a65f
Sterling Woodruff <sterlingwoodruff@mail.weber.edu>
2017-03-28 19:40:03 +0000

    task 2 first commit & gitignore

--- romulan_hw8_task2.c
+++ romulan_hw8_task2.c
@@ -13,16 +13,42 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#include <string.h>		/* For String Library */
+
 /* Function Prototypes */
+void Usage(char** info);
+//void GetOptions(char );
+//void PrintOption(char );
 
 /* Main Program */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	// if does not equal 1 parameter, call usage
+	if (argc != 2)
+	{
+		printf("Missing or wrong number of parameters\n");
+		Usage(argv);
+	}
+	// if first input is --help, call Usage()
+	else if (strcmp(argv[1],"--help") == 0)
+	{
+		printf("Help information\n");
+		Usage(argv);
+	}
 
 	return 0;
 }
 
 
 /* Function Defenitions */
-
-
+void Usage(char** info)
+{
+	printf("Usage %s [-p | -u | -l]\n", *info);
+	exit(1);
+	return;
+}
+/*char StringIn(char* str1, char* str2)
+{
+	exit(1);
+	return '0';
+}*/
