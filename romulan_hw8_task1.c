52ba9a660df9d7263584944c86bf43e305f80ed1
Chris Werner <chris.lex.werner@gmail.com>
2017-03-28 03:39:54 +0000

    Usage function and initial input validation is working. Task 1.

--- romulan_hw8_task1.c
+++ romulan_hw8_task1.c
@@ -13,17 +13,49 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>		/* For Standard Library*/
+#include <string.h>		/* For String library */
 
 /* Function Prototypes */
+void Usage(char** info);
+char StringIn(char* str1, char* str2);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	// if input equals 1 or 4 or more parameters, call usage
+	if (argc == 1 || argc >= 4)
+	{
+		printf("Missing required parameters\n");
+		Usage(argv);
+	}
+	// if first input is --help, call Usage()
+	else if (strcmp(argv[1],"--help") == 0)
+	{
+		printf("Help information\n");
+		Usage(argv);
+	}
+	// if input equals 2 paramters (but NOT --help as 2nd) then call usage
+	else if (argc == 2)
+	{
+		printf("Missing required paramters\n");
+		Usage(argv);
+	}
+	
 
 	return 0;
 }
 
 
 /* Function Defenitions */
-
-
+void Usage(char** info)
+{
+	printf("Usage ./romulan_hw8_task1 <str1> <str2>\n");
+	printf("Program checks if str2 is part of str1\n");
+	exit(1);
+	return;
+}
+char StringIn(char* str1, char* str2)
+{
+	exit(1);
+	return '0';
+}
