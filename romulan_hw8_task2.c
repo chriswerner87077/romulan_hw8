546c161f94b2fea1d3d01d50ddecdf6856d16200
Chris Werner <chris.lex.werner@gmail.com>
2017-03-29 02:54:34 +0000

    Got it to work correctly. But not yet complete. The modifying of the text was done (incorrectly) in the GetOptions function rather than the PrintOption() function. Shoudl be a simple copy/paste. Also, the getoptions function needs to return the option (p, u, or l) that the user selected and pass it to the PrintOption function.

--- romulan_hw8_task2.c
+++ romulan_hw8_task2.c
@@ -14,7 +14,9 @@
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
 #include <string.h>		/* For String Library */
+#include <ctype.h>		/* For Character Library */
 
+#define MAXCHARS 100
 /* Function Prototypes */
 void Usage(char** info);
 void GetOptions(char *argv);
@@ -51,15 +53,47 @@ void Usage(char** info)
 }
 void GetOptions(char *argv)
 {
+	char str1[MAXCHARS];
+	char str2[MAXCHARS];
+	int i = 0;
+	float marker;
+	// print the selected option and default if incorrect option is used
 	if(strcmp(argv, "-p") == 0 || strcmp(argv, "-u") == 0 || strcmp(argv, "-l") == 0)
 	{
-		
+		printf("Selecting option %s.\n", argv);
 	}
 	else
 	{
-		printf("%s is an in)valid flag; using default flag (-p).\n", argv);
+		printf("%s is an invalid flag; using default flag (-p).\n", argv);
 	}
-
+	// prompt user for input
+	printf("Type input. Output will be based on option.\n");
+	printf("Enter Ctrl-D to stop user input: ");
+	// while loop to get adn process input, only exits when Ctrl-D entered
+	while (scanf("%f", &marker) != EOF)
+	{
+		// while loop to capture each character
+		while ((str1[i++] = getchar()) != '\n')
+			;
+		i = 0;
+		while (str1[i])
+		{
+			if (strcmp(argv,"-u") == 0)
+			{
+				str2[i] = toupper(str1[i]);
+			}
+			else if (strcmp(argv, "-l") == 0) 
+			{
+				str2[i] = tolower(str1[i]);
+			}
+			else
+			{
+				str2[i] = str1[i];
+			}
+			printf("%c", str2[i]);
+			i++;
+		}
+	}
 	return;
 }
 
